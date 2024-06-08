9312342f3c0379f1fbfa43aaacbabdd714c915c2
2gutierrez <159669831+2gutierrez@users.noreply.github.com>
2024-06-08 12:23:35 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,61 @@
+const campoTexto = document.querySelector("#textoIntroducido");
+const resultado = document.querySelector("#resultado");
+const errorMessage = document.querySelector("#error-message");
+
+const matriz = [
+  ["e", "enter"],
+  ["i", "imes"],
+  ["a", "ai"],
+  ["o", "ober"],
+  ["u", "ufat"]
+];
+
 function botonE() {
-    const input = document.getElementById('textoIntroducido').value;
-    const output = encode(input);
-    document.getElementById('resultado').value = output;
+  const inputText = campoTexto.value;
+  if (/[A-Z]/.test(inputText) || /[^a-z\s]/.test(inputText)) {
+    errorMessage.textContent = "Error: No usar letras mayúsculas ni caracteres especiales.";
+    errorMessage.style.display = "block";
+    return;
+  }
+  errorMessage.style.display = "none";
+  const texto = encriptar(inputText);
+  resultado.value = texto;
+}
+
+function encriptar(fraseEncriptada) {
+  for (let i = 0; i < matriz.length; i++) {
+    if (fraseEncriptada.includes(matriz[i][0])) {
+      fraseEncriptada = fraseEncriptada.replaceAll(matriz[i][0], matriz[i][1]);
+    }
+  }
+  return fraseEncriptada;
 }
 
 function botonD() {
-    const input = document.getElementById('textoIntroducido').value;
-    const output = decode(input);
-    document.getElementById('resultado').value = output;
+  const inputText = campoTexto.value;
+  if (/[A-Z]/.test(inputText) || /[^a-z\s]/.test(inputText)) {
+    errorMessage.textContent = "Error: No usar letras mayúsculas ni caracteres especiales.";
+    errorMessage.style.display = "block";
+    return;
+  }
+  errorMessage.style.display = "none";
+  const texto = desencriptar(inputText);
+  resultado.value = texto;
+}
+
+function desencriptar(fraseDesEncriptada) {
+  for (let i = 0; i < matriz.length; i++) {
+    if (fraseDesEncriptada.includes(matriz[i][1])) {
+      fraseDesEncriptada = fraseDesEncriptada.replaceAll(matriz[i][1], matriz[i][0]);
+    }
+  }
+  return fraseDesEncriptada;
 }
 
 function botonC() {
-    const result = document.getElementById('resultado');
-    result.select();
-    document.execCommand('copy');
-    alert('Texto copiado al portapapeles');
+  const copiarTexto = document.getElementById("resultado");
+  copiarTexto.select();
+  document.execCommand("copy");
+  alert("Texto copiado al portapapeles");
 }
 
-function encode(text) {
-    // Implement your encoding logic here
-    return text.split('').reverse().join(''); // Example: reversing the text
-}
-
-function decode(text) {
-    // Implement your decoding logic here
-    return text.split('').reverse().join(''); // Example: reversing the text back
-}
